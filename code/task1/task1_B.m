51cf8a9facf538354c48efe5853510afe2a39af1
Mani Abedii <maniabedii.ce@gmail.com>
2025-07-17 22:19:38 +0430

    add task1 - B code

---
+++ code/task1/task1_B.m
@@ -0,0 +1,21 @@
+Fs = 5000;                     % Sampling frequency
+t = 0:1/Fs:0.01;               % Time vector (10 ms)
+
+freqs = [200, 400, 600];
+amps = [1, 0.5, 0.2];
+
+disp('Playing sounds...');
+
+for i = 1:length(freqs)
+
+    % Generate sinusoid
+    x = amps(i) * sin(2*pi*freqs(i)*t);
+    
+    % Normalize to prevent clipping
+    x = x / max(abs(x));
+    
+    % Playback
+    disp(['Playing ', num2str(freqs(i)), ' Hz...']);
+    sound(x, Fs);
+    pause(1.5);
+end
